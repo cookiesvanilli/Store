33547afc11dedc54bbdf28959cccac138713367b
Inna K <kuklina.inn@yandex.ru>
2022-08-27 20:26:02 +0300

    add bonus

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static String[] products = { "Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка" };
-    public static int[] prices = { 50, 135, 65, 53 };
+    public static String[] products = {"Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка"};
+    public static int[] prices = {50, 135, 65, 53};
+
+    public static int MIN_COST_FOR_BONUS = 1000;
 
     // В стоимости этих товаров каждые три товара должны стоить как два:
-    public static String[] productsOnSale = { "Хлеб", "Мороженка" };
+    public static String[] productsOnSale = {"Хлеб", "Мороженка"};
 
     public static void main(String[] args) {
+        int amount = 0;
         System.out.println("Добро пожаловать в магазин!");
         System.out.println("Наш ассортимент:");
         for (int i = 0; i < products.length; i++) {
@@ -28,15 +31,33 @@ public class Main {
                 break;
             }
 
-            String[] parts = line.split(" ");
-            int productNum = Integer.parseInt(parts[0]) - 1;
-            int productCount = Integer.parseInt(parts[1]);
+            try {
+                String[] parts = line.split(" ");
+                if (parts.length != 2) {
+                    System.out.println("Некорректный ввод данных.");
+                    continue;
+                } else {
+                    int productNum = Integer.parseInt(parts[0]) - 1;
+                    int productCount = Integer.parseInt(parts[1]);
 
-            counts[productNum] += productCount;
+                    counts[productNum] += productCount;
+                }
+
+            } catch (NumberFormatException exception) {
+                System.out.println("Некорректные данные!");
+                continue;
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                System.out.println("Некорректные данные!");
+                continue;
+            }
         }
 
         System.out.println("Ваша корзина покупок:");
         int sum = 0;
+        for (int i = 0; i < products.length; i++) {
+            sum += prices[i] * counts[i];
+        }
+        boolean doBonus = sum >= MIN_COST_FOR_BONUS;
         for (int i = 0; i < products.length; i++) {
             if (counts[i] != 0) {
                 boolean isOnSale = false;
@@ -45,17 +66,22 @@ public class Main {
                         isOnSale = true;
                     }
                 }
+                StringBuilder str = new StringBuilder();
+                int c = (doBonus ? counts[i] + 1 : counts[i]);
 
-                if (isOnSale) {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
-                    sum += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
+                if (isOnSale && doBonus) {
+                    System.out.println("\t" + products[i] + " " + str.append(c) + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа и акция!)");
+                    amount += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
+                } else if (isOnSale) {
+                    System.out.println("\t" + products[i] + " " + str.append(c) + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
+                    amount += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
                 } else {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
-                    sum += prices[i] * counts[i];
+                    System.out.println("\t" + products[i] + " " + str.append(c) + " шт. за " + (prices[i] * counts[i]) + " руб.");
+                    amount += prices[i] * counts[i];
                 }
             }
         }
-        System.out.println("Итого: " + sum + " руб.");
+        System.out.println("Итого: " + amount + " руб.");
     }
 
 }
